7d4e08bc9e13c9f5de17c267db1202e88b06a1fe
Aquiles Carattino <aquiles@aquicarattino.com>
2019-04-09 19:16:31 +0200

    Beginning of Day 2

--- PFTL/test_default_args.py
+++
@@ -1,9 +0,0 @@
-def print_full_name(name='Aquiles', last_name='Carattino'):
-    print(name, last_name)
-
-print_full_name(last_name='Other')
-
-
-def print_other_full_name(name, last_name, middle_name):
-    print(name, last_name)
-
